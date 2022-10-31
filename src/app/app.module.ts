244286a95e410360cefb2016c041437936ef2a8f
Maskooth Farook <88648646+farookdarkcode@users.noreply.github.com>
2022-10-31 12:25:31 +0530

    Updated Files

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginpageComponent } from './login/loginpage/loginpage.component';
+import { SignupComponent } from './login/signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginpageComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
