757b5ae574e7033e796bcfac2da4f200043da5a2
Vivek Kumar <Vivekkumar-R@lntecc.com>
2022-11-19 09:46:06 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://authedgeservicedev.lntedutech.com',
-  API_UPLOAD_URL: 'https://authedgeservicedev.lntedutech.com',
+  API_BASE_URL: 'https://authedgeserviceqa.lntedutech.com',
+  API_UPLOAD_URL: 'https://authedgeserviceqa.lntedutech.com',
 };
 
 
