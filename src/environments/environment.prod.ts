eb079c1e2498e3ebf2e6d0d4fb1e4c63256e96b3
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2022-12-02 17:35:22 +0530

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   API_BASE_URL: 'https://authedgeserviceqa.lntedutech.com',
   API_UPLOAD_URL: 'https://authedgeserviceqa.lntedutech.com',
+  API_TAO_URL:'https://authedgeserviceqa.lntedutech.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: true,
   API_BASE_URL: 'https://authedgeserviceqa.lntedutech.com',
   API_UPLOAD_URL: 'https://authedgeserviceqa.lntedutech.com',
+  API_TAO_URL:'https://authedgeserviceqa.lntedutech.com',
 };
 
 
