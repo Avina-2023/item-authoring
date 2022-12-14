7e7242d5605877d9b59b0a7834a277bd98117a19
Maskooth Farook <88648646+farookdarkcode@users.noreply.github.com>
2022-12-14 12:08:12 +0530

    updated Tao sync URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   API_BASE_URL: 'https://authedgeservicedev.lntedutech.com',
-   API_TAO_URL: 'https://authedgeservicedev.lntedutech.com',
+  API_TAO_URL: 'https://authedgeservicedev.lntedutech.com',
+  SOCKET_ENDPOINT: 'https://authedgeservicedev.lntedutech.com',
 };
