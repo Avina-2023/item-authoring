a160079a239ae0474dfc1fc72b7e3310a59dcc3f
Maskooth Farook <maskooth.bavabahrudeen@lntecc.com>
2022-12-15 12:09:25 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
   API_BASE_URL: 'https://authedgeserviceqa.lntedutech.com',
-  API_UPLOAD_URL: 'https://authedgeserviceqa.lntedutech.com',
-  API_TAO_URL: 'https://authedgeserviceqa.lntedutech.com',
-  SOCKET_ENDPOINT: 'https://authedgeserviceqa.lntedutech.com',
 };
