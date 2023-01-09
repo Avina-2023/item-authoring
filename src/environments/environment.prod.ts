880e3184b91a03e8e65174ddfebcc84597dcf3e3
Maskooth Farook <88648646+farookdarkcode@users.noreply.github.com>
2023-01-09 14:26:42 +0530

    Environment URL Updated
    Environment URL Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://authedgeservice.lntedutech.com',
-  API_UPLOAD_URL: 'https://authedgeservice.lntedutech.com',
-  API_TAO_URL: 'https://authedgeservice.lntedutech.com',
-  SOCKET_ENDPOINT: 'https://authedgeservice.lntedutech.com',
+  API_BASE_URL: 'https://itemauthedge.lntedutech.com',
+  API_UPLOAD_URL: 'https://itemauthedge.lntedutech.com',
+  API_TAO_URL: 'https://itemauthedge.lntedutech.com',
+  SOCKET_ENDPOINT: 'https://itemauthedge.lntedutech.com',
 };
