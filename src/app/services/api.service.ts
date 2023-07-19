fe608def6f4f0f68c02566339bc55ba92ae7039d
murali <murali.b@lntecc.com>
2023-07-19 17:42:55 +0530

    remove close icon error message
    remove close icon  error message

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -27,9 +27,13 @@ export class ApiService {
   uploaded(file: any):Observable<any> {
     return this.http.post(`${this.BASE_URL}/uploadQuestion`, file).pipe(catchError((error: HttpErrorResponse) => {
       if (error.status === 400) {
-        this.toastr.error('Bad Request: ' + error.error.message, 'Error');
+        this.toastr.error('Bad Request: ' + error.error.message, 'Error',{
+          closeButton:false
+        });
       } else {
-        this.toastr.error('An error occurred: ' + error.error.message, 'Error');
+        this.toastr.error('An error occurred: ' + error.error.message, 'Error',{
+          closeButton:false
+        });
       }
       return throwError('Something went wrong; please try again later.');
     }));
