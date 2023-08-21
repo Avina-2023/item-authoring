59bd61fb26a7e69067bed84fa09a495c0fe5595a
VIVEK KUMAR <vivek.kumar12@lntecc.com>
2023-08-21 06:15:08 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://itemauthedge.lntedutech.com',
-  API_UPLOAD_URL: 'https://itemauthedge.lntedutech.com',
-  API_TAO_URL: 'https://itemauthedge.lntedutech.com',
-  SOCKET_ENDPOINT: 'https://itemauthedge.lntedutech.com',
+  API_BASE_URL: 'https://itemauthedgeprod.lntedutech.com',
+  API_UPLOAD_URL: 'https://itemauthedgeprod.lntedutech.com',
+  API_TAO_URL: 'https://itemauthedgeprod.lntedutech.com',
+  SOCKET_ENDPOINT: 'https://itemauthedgeprod.lntedutech.com',
 };
